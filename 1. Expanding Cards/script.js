8935b5a608a48940e7922019e501b1102802a7b0
Pranav Nandane <41197788+pranavnan@users.noreply.github.com>
2023-01-03 20:20:57 +0530

    Add files via upload

---
+++ 1. Expanding Cards/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const panels = document.querySelectorAll(".panel");
+console.log(panels);
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", function () {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
